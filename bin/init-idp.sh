eb12eae0c3bf3b001086d36a84a108cc99164fd9
Mark van Rossum <mark.vanrossum@bristol.ac.uk>
2021-01-08 10:11:55 +0000

    mostly working

--- bin/init-idp.sh
+++ bin/init-idp.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 
-export JAVA_HOME=/opt/jre-home
-export PATH=$PATH:$JAVA_HOME/bin
-
-cd /opt/shibboleth-idp/bin
+EXPORT_DIR=/ext-mount
+TARGET_DIR=/opt/shibboleth-idp-tmp
 
 echo "Please complete the following for your IdP environment:"
-./ant.sh -Didp.target.dir=/opt/shibboleth-idp-tmp -Didp.src.dir=/opt/shibboleth-idp/ install
+$IDP_SRC/bin/install.sh -Didp.target.dir=/opt/shibboleth-idp-tmp -Didp.src.dir=$IDP_SRC install
 
-find /opt/shibboleth-idp-tmp/ -type d -exec chmod 750 {} \;
+# find /opt/shibboleth-idp-tmp/ -type d -exec chmod 750 {} \;
 
-mkdir -p /ext-mount/customized-shibboleth-idp/conf/
-chmod -R 750 /ext-mount/customized-shibboleth-idp/
+#mkdir -p $EXPORT_DIR/
+#chmod -R 750 $EXPORT_DIR/
 
-# Copy the essential and routinely customized config to out Docker mount.
-cd /opt/shibboleth-idp-tmp
-cp -r credentials/ /ext-mount/customized-shibboleth-idp/
-cp -r metadata/ /ext-mount/customized-shibboleth-idp/
-cp conf/{attribute-resolver*.xml,attribute-filter.xml,cas-protocol.xml,idp.properties,ldap.properties,metadata-providers.xml,relying-party.xml,saml-nameid.*} /ext-mount/customized-shibboleth-idp/conf/
+# Copy config to export directory
+cp -r $TARGET_DIR/credentials/ $EXPORT_DIR/
+cp -r $TARGET_DIR/metadata/ $EXPORT_DIR/
+cp -r $TARGET_DIR/conf/ $EXPORT_DIR/
+cp -r $TARGET_DIR/views/ $EXPORT_DIR/
+cp -r $TARGET_DIR/edit-webapp/ $EXPORT_DIR/
 
-# Copy the basic UI components, which are routinely customized
-cp -r views/ /ext-mount/customized-shibboleth-idp/
-mkdir /ext-mount/customized-shibboleth-idp/webapp/
-cp -r edit-webapp/css/ /ext-mount/customized-shibboleth-idp/webapp/
-cp -r edit-webapp/images/ /ext-mount/customized-shibboleth-idp/webapp/
-rm -r /ext-mount/customized-shibboleth-idp/views/user-prefs.js
 
-echo "A basic Shibboleth IdP config and UI has been copied to ./customized-shibboleth-idp/ (assuming the default volume mapping was used)."
-echo "Most files, if not being customized can be removed from what was exported/the local Docker image and baseline files will be used."
+echo "User editable config files and UI have been copied to ./customized-shibboleth-idp/ (assuming the default volume mapping was used)."
+echo "Files that are not customized can be removed from what was exported, the default file in the Docker image will be used instead"
--- bin/run-jetty.sh
+++ bin/run-jetty.sh
@@ -1,16 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-#set -x
-
-export JAVA_HOME=/opt/jre-home
-export PATH=$PATH:$JAVA_HOME/bin
-
-if [ -e "/opt/shibboleth-idp/ext-conf/idp-secrets.properties" ]; then
-  export JETTY_BACKCHANNEL_SSL_KEYSTORE_PASSWORD=`gawk 'match($0,/^jetty.backchannel.sslContext.keyStorePassword=\s?(.*)\s?$/, a) {print a[1]}' /opt/shibboleth-idp/ext-conf/idp-secrets.properties`
-  export JETTY_BROWSER_SSL_KEYSTORE_PASSWORD=`gawk 'match($0,/^jetty\.sslContext\.keyStorePassword=\s?(.*)\s?$/, a) {print a[1]}' /opt/shibboleth-idp/ext-conf/idp-secrets.properties`
-fi
-
-export JETTY_ARGS="jetty.sslContext.keyStorePassword=$JETTY_BROWSER_SSL_KEYSTORE_PASSWORD jetty.backchannel.sslContext.keyStorePassword=$JETTY_BACKCHANNEL_SSL_KEYSTORE_PASSWORD"
-sed -i "s/^-Xmx.*$/-Xmx$JETTY_MAX_HEAP/g" /opt/shib-jetty-base/start.ini
+export JETTY_ARGS="\
+        jetty.ssl.port=4443 \
+        idp.backchannel.keyStorePath=$JETTY_BACKCHANNEL_SSL_KEYSTORE_PATH \
+        idp.backchannel.keyStorePassword=$JETTY_BACKCHANNEL_SSL_KEYSTORE_PASSWORD \
+        jetty.sslContext.keyStorePath=$JETTY_BROWSER_SSL_KEYSTORE_PATH \
+        jetty.sslContext.trustStorePath=$JETTY_BROWSER_SSL_KEYSTORE_PATH \
+        jetty.sslContext.keyStorePassword=$JETTY_BACKCHANNEL_SSL_KEYSTORE_PASSWORD \
+        jetty.sslContext.trustStorePassword=$JETTY_BACKCHANNEL_SSL_KEYSTORE_PASSWORD \
+        jetty.sslContext.keyManagerPassword=$JETTY_BACKCHANNEL_SSL_KEYSTORE_PASSWORD"
+        
+export _JAVA_OPTIONS="-Didp.war.path=$IDP_HOME/war/idp.war -Xmx$JETTY_MAX_HEAP"
 
 exec /opt/jetty-home/bin/jetty.sh run
